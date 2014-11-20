1932b15e8159d63cc21dca56cfb27bf17bd0ba3d
Andrew Consroe <aconz2@umiacs.umd.edu>
2014-11-20 12:19:13 -0500

    undid moving things into seperate classes, not very useful. problem was not including num_threads in declaring mer_hash for future reference

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC = g++
 #CXXFLAGS = -Wall -Werror -O3 $(shell pkg-config --cflags jellyfish-2.0) -std=c++0x
 # remove -Werror for weird warnings from boost 
-CXXFLAGS = -Wall -Werror -Wunused-local-typedefs -O3 $(shell pkg-config --cflags jellyfish-2.0) -std=c++11
+CXXFLAGS = -Wall -Werror -Wunused-local-typedefs -O3 $(shell pkg-config --cflags jellyfish-2.0) -std=c++0x
 # this should get changed for portability sometime!
 LDFLAGS = $(shell pkg-config --libs-only-L jellyfish-2.0) -L$(BOOST_ROOT)/lib 
-LDLIBS = $(shell pkg-config --libs-only-l jellyfish-2.0) -lboost_timer -lboost_chrono -lboost_system -lboost_program_options
+LDLIBS = -lboost_timer -lboost_chrono -lboost_system -lboost_program_options $(shell pkg-config --libs-only-l jellyfish-2.0) 
 
 all: count
-count: count.cc JellyfishHash.hpp
+count: count.cc 
 clean:
 	rm -f *.o count
--- count.cc
+++ count.cc
@@ -6,15 +6,64 @@
 #include <boost/timer/timer.hpp>
 #include <boost/program_options.hpp>
 
-#include "JellyfishHash.hpp"
+#include <jellyfish/err.hpp>
+#include <jellyfish/thread_exec.hpp>
+#include <jellyfish/hash_counter.hpp>
+#include <jellyfish/stream_manager.hpp>
+#include <jellyfish/mer_overlap_sequence_parser.hpp>
+#include <jellyfish/whole_sequence_parser.hpp>
+#include <jellyfish/mer_iterator.hpp>
+#include <jellyfish/mer_dna.hpp>
+#include <jellyfish/jellyfish.hpp>
+#include <jellyfish/large_hash_iterator.hpp>
 
+using jellyfish::mer_dna;
+typedef jellyfish::stream_manager<char**> stream_manager;
+typedef jellyfish::mer_overlap_sequence_parser<jellyfish::stream_manager<char**>> sequence_parser;
+typedef jellyfish::mer_iterator<sequence_parser, mer_dna> mer_iterator;
+typedef jellyfish::whole_sequence_parser<jellyfish::stream_manager<char**>> read_parser;
+ 
+/* ========== mer_counter ========== */
+class mer_counter : public jellyfish::thread_exec {
+  bool            canonical_;
+  int             nb_threads_;
+  mer_hash&       ary_;
+  stream_manager  streams_;
+  sequence_parser parser_;
+
+public:
+  mer_counter(int nb_threads, mer_hash& ary, char** file_begin, char** file_end, bool canonical) :
+    canonical_(canonical),
+    nb_threads_(nb_threads),
+    ary_(ary),
+    streams_(file_begin, file_end, 1), // 1: parse one file at a time
+    parser_(mer_dna::k(), streams_.nb_streams(), 3 * nb_threads, 4096, streams_)
+  { }
+
+  virtual void start(int thid) {
+    for(mer_iterator mers(parser_, canonical_) ; mers; ++mers) {
+      ary_.add(*mers, 1);
+    }
+    ary_.done();
+  }
+
+};
+
+/* ========== main ========== */
 namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
 
+  /* provided by Guillaume */
+  const int counter_length = 7;
+  const int max_reprobe = 126;
+  const bool canonical = false;
+
+  /* default values */
   bool kmer_stats = false;
   int num_threads = 1;
 
+  /* manadatory arguments */
   int kmer_length;
   unsigned long hash_size;
   std::string reads;
@@ -28,7 +77,7 @@ int main(int argc, char *argv[]) {
     ("reads,r", po::value<std::string>(&reads)->required(), "Reads from sample, fast(a|q)")
     ("genes,g", po::value<std::string>(&genes)->required(), "Genes file, fasta")
     ("threads,t", po::value<int>(&num_threads)->default_value(1), "Number of threads to use")
-    ("debug,d", po::value<bool>(&kmer_stats)->default_value(false), "Print max count, total, and unique kmers from Jellyfish");
+    ("kmer-stats", po::value<bool>(&kmer_stats)->default_value(false), "Print max count, total, and unique kmers from Jellyfish (Adds large time cost)");
 
   po::variables_map vm;
   try {
@@ -44,34 +93,60 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+  std::cerr << "Starting clownfish with: kmer_length=" << kmer_length << 
+                                         ",hash_size=" << hash_size <<
+                                             ",reads=" << reads <<
+                                             ",genes=" << genes <<
+                                           ",threads=" << num_threads <<
+                                         ",kmer_stats=" << kmer_stats << std::endl;
+
   mer_dna::k(kmer_length);
-  jelly_hash hash(hash_size, kmer_length, num_threads, reads);
+  mer_hash hash(hash_size, mer_dna::k() * 2, counter_length, num_threads, max_reprobe);
 
   std::ifstream genes_fs(genes);
   std::cout.precision(20);
 
+  /* ---------- Fill Jellyfish Hashtable ---------- */
   {
     boost::timer::auto_cpu_timer t(std::cerr, 2);
     std::cerr << "=== Filling jellyfish hash ===" << std::endl;
-    hash.fill();
+    // jellyfish likes c style strings 'n stuff
+    char **reads_c = new char*[1];
+    reads_c[0] = (char *) reads.c_str();
+
+    mer_counter counter(num_threads, hash, reads_c, reads_c + 1, canonical);
+    counter.exec_join(num_threads);
+
+    delete[] reads_c;
   }
 
+  mer_array* ary = hash.ary();
+
+  /* ---------- Calculate kmer stats ---------- */
   if(kmer_stats) {
     boost::timer::auto_cpu_timer t(std::cerr, 2); 
     std::cerr << "=== Calcuating kmer stats ===" << std::endl;
-    uint64_t distinct;
-    uint64_t max;
-    uint64_t total;
-    hash.stats(distinct, max, total);
+    uint64_t max = 0;
+    uint64_t total = 0;
+    uint64_t distinct = 0;
+    uint64_t val = 0;    
+    for(auto it = ary->begin(); it != ary->end(); ++it) {
+      val = it->second;
+      distinct++;
+      total += val;
+      max = std::max(val, max);
+    }
     std::cerr << "distinct:" << distinct << std::endl;
     std::cerr << "max:" << max << std::endl;
     std::cerr << "total:" << total << std::endl;
   }
 
+  /* ---------- Count kmers in genes ---------- */
   {
     boost::timer::auto_cpu_timer t(std::cerr, 2); 
     std::cerr << "=== Counting genes ===" << std::endl;
     
+    mer_dna mer;
     std::string buf; 
     // skip the first line
     std::getline(genes_fs, buf); 
@@ -88,17 +163,19 @@ int main(int argc, char *argv[]) {
       } while(buf[0] != '>' && !genes_fs.eof());
 
      uint64_t sum = 0;
+     uint64_t val;
      int length = gene.size() - kmer_length + 1;
       for(int i = 0; i < length; ++i) {
-        std::string kmer = gene.substr(i, kmer_length);
-        sum += hash.get(kmer);
-        //std::cerr << val << ' ';
-        //output_fs.write(reinterpret_cast<const char *>(&val), sizeof(uint32_t));
+        mer = gene.substr(i, kmer_length);
+        if(!ary->get_val_for_key(mer, &val)) {
+          val = 0;
+        }
+        sum += val;
       }
       std::cout << (double) sum / length << std::endl;
     }
   }
 
-  std::cerr << "=== Exiting clownfish count ===" << std::endl;
+  //std::cerr << "=== Exiting clownfish count ===" << std::endl;
   return 0;
 }
